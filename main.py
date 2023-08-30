32253d6815b87bec4cc17d5ad4b963a754275062
mick <alfred.scott@protonmail.com>
2023-08-30 16:20:41 -0400

    transaction data partially parsed

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import json
 import xmltodict
 
-with open('a_c_s.gnucash', 'r') as gnucash_data:
+with open('crypto_test.gnucash', 'r') as gnucash_data:
     gnucash_data_dict = xmltodict.parse(gnucash_data.read())
+
+print(gnucash_data_dict['gnc-v2']['gnc:book']['gnc:transaction'])
+
+for transaction in gnucash_data_dict['gnc-v2']['gnc:book']['gnc:transaction']:
+    print(transaction)
+    print(transaction['trn:id'])
+    print(transaction['trn:currency'])
+    print(transaction['trn:date-posted'])
+    print(transaction['trn:date-entered'])
+    print(transaction['trn:description'])
+    print(transaction['trn:slots'])
+    print(transaction['trn:splits'])
 
 gnucash_json = json.dumps(gnucash_data_dict, indent=4)
 
