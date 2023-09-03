4805a197e1a7dd3e2005e04083e8536dbbcfeb81
mick <alfred.scott@protonmail.com>
2023-09-03 11:53:13 -0400

    updates

--- main.py
+++ main.py
@@ -17,12 +17,22 @@ if from_terminal:
 
 trans_account_id_list = []
 act_list = make_account_list(gnucash_data_dict)
-transaction_list = make_transaction_list(gnucash_data_dict, act_list)
+trn_list = make_transaction_list(gnucash_data_dict, act_list)
 
-for trn in transaction_list:
-    print(trn)
+act_dict = {"accounts": act_list}
+trn_dict = {"transactions": trn_list}
 
 gnucash_json = json.dumps(gnucash_data_dict, indent=4)
-
-with open("data.json", "w") as json_file:
+with open('gnucash_xml.json', 'w') as json_file:
     json_file.write(gnucash_json)
+    json_file.close()
+
+trn_json = json.dumps(trn_dict, indent=4)
+with open('trn.json', 'w') as json_file:
+    json_file.write(trn_json)
+    json_file.close()
+
+act_json = json.dumps(act_dict, indent=4)
+with open('act.json', 'w') as json_file:
+    json_file.write(act_json)
+    json_file.close()
